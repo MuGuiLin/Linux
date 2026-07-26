fd32ac8ce0360332794847cca9c0ae70d0158dda
muguilin <muguilin@foxmail.com>
2026-07-26 15:54:08 +0800

    update

---
+++ Bat批处理脚本/微信多开.bat
@@ -0,0 +1,4 @@
+::本批处理由https://github.com/MuGuiLin生成
+start D:\"Program Files"\Tencent\Weixin\Weixin.exe
+start D:\"Program Files"\Tencent\Weixin\Weixin.exe
+start D:\"Program Files"\Tencent\Weixin\Weixin.exe
